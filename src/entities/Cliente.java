689b0036db67189bb897ab410a69d0c2a36e7cf3
Eduardo <eduardojor77@gmail.com>
2023-11-27 14:44:16 -0300

    classe Cliente criada

---
+++ src/entities/Cliente.java
@@ -0,0 +1,69 @@
+package entities;
+
+import java.util.ArrayList;
+
+public class Cliente {
+    private String nome;
+    private String cpf;
+    private String email;
+    private String telefone;
+    private String endereco;
+    private ArrayList<Produto> carrinho;
+
+    public Cliente(String nome, String cpf, String email, String telefone, String endereco) {
+        this.nome = nome;
+        this.cpf = cpf;
+        this.email = email;
+        this.telefone = telefone;
+        this.endereco = endereco;
+        this.carrinho = new ArrayList<>();
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public ArrayList<Produto> getCarrinho() {
+        return carrinho;
+    }
+
+    public void setCarrinho(ArrayList<Produto> carrinho) {
+        this.carrinho = carrinho;
+    }
+
+    public void adicionarAoCarrinho(Produto produto) {
+        carrinho.add(produto);
+    }
+}
