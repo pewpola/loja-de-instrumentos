9409afd3a36d659c924297965ddfcb5b4d21bb85
Eduardo <eduardojor77@gmail.com>
2023-11-27 14:02:38 -0300

    adicao da classe e interface produto

---
+++ src/interfaces/InterfaceProduto.java
@@ -0,0 +1,8 @@
+package interfaces;
+
+public interface InterfaceProduto {
+    public String getNome();
+    public String getMarca();
+    public double getPreco();
+    public String getDescricao();
+}
