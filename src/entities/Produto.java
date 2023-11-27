5f26ec1dd7a942ae3f6e180ecbdc470730191b8b
Eduardo <eduardojor77@gmail.com>
2023-11-27 14:10:13 -0300

    adicao dos atributos e métodos a classe produto

--- src/entities/Produto.java
+++ src/entities/Produto.java
@@ -1,5 +1,37 @@
 package entities;
 
-public class Produto {
+import interfaces.InterfaceProduto;
 
+abstract public class Produto implements InterfaceProduto {
+    private String nome;
+    private String marca;
+    private double preco;
+    private String descricao;
+
+    public Produto(String nome, String marca, double preco, String descricao) {
+        this.nome = nome;
+        this.marca = marca;
+        this.preco = preco;
+        this.descricao = descricao;
+    }
+
+    @Override
+    public String getNome() {
+        return this.nome;
+    }
+
+    @Override
+    public String getMarca() {
+        return this.marca;
+    }
+
+    @Override
+    public double getPreco() {
+        return this.preco;
+    }
+
+    @Override
+    public String getDescricao() {
+        return this.descricao;
+    }
 }
